da2f178e525ca7d8a4717656a1cbbaa84c255bb1
HastagKc <chaudharykshittiz950@gmail.com>
2024-06-23 02:14:15 +0545

    validateing multiple fields

--- djangoFormFive/main/forms.py
+++ djangoFormFive/main/forms.py
@@ -7,9 +7,23 @@ class StudentRegistration(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
 
-    # validation specifies files
-    def clean_age(self):
-        age_data = self.cleaned_data['age']
-        if age_data < 0:
-            raise forms.ValidationError("Age can't be negative ")
-        return age_data
+    # validation specifies field
+    # def clean_age(self):
+    #     age_data = self.cleaned_data['age']
+    #     if age_data < 0:
+    #         raise forms.ValidationError("Age can't be negative ")
+    #     return age_data
+
+ # Validate all fields
+    def clean(self):
+        cleaned_data = super().clean()
+        valname = cleaned_data.get('name')
+        valage = cleaned_data.get('age')
+
+        if valname and len(valname) < 2:
+            raise forms.ValidationError("Name can't be less than 2 characters")
+
+        if valage is not None and valage < 0:
+            raise forms.ValidationError("Age can't be negative")
+
+        return cleaned_data
